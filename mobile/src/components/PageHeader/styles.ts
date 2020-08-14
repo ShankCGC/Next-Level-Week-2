aea6292d373a49d49024127321e80c5cd40d4f6b
ShankCGC <shankcgc@gmail.com>
2020-08-14 12:01:08 -0300

    Adding a button to filter teachers and connecting with the Backend

--- mobile/src/components/PageHeader/styles.ts
+++ mobile/src/components/PageHeader/styles.ts
@@ -13,6 +13,12 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between'
   },
 
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+
   title: {
     fontFamily: 'Archivo_700Bold',
     color: '#FFF',
@@ -20,7 +26,7 @@ const styles = StyleSheet.create({
     lineHeight: 32,
     maxWidth: 160,
     marginVertical: 40
-  }
+  },
 })
 
 export default styles
--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -8,23 +8,37 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
 import styles from './styles'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number
+  avatar: string
+  bio: string
+  cost: number
+  name: string
+  subject: string,
+  whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
-        <Image style={styles.avatar} source={{ uri: 'https://github.com/ShankCGC.png' }} />
+        <Image style={styles.avatar} source={{ uri: teacher.avatar }} />
 
         <View style={styles.profileInfo}>
-          <Text style={styles.name}>ShankCGC</Text>
-          <Text style={styles.subject}>Geografia</Text> 
+          <Text style={styles.name}>{teacher.name}</Text>
+          <Text style={styles.subject}>{teacher.subject}</Text> 
         </View>
       </View>
 
-      <Text style={styles.bio}>Apenas um teste amigo {'\n'}{'\n'}UM TESTE!</Text>
+      <Text style={styles.bio}>{teacher.bio}</Text>
 
       <View style={styles.footer}>
         <Text style={styles.price}>Preço/hora {'   '}
-          <Text style={styles.priceValue}>R$20,00</Text>
+          <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
         </Text>
 
         <View style={styles.buttonsContainer}>
